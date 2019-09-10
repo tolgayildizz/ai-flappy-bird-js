b5e0ab4e6071426cc90f194751a4016f780223ff
tolgayildizz <yildiztolgaa@gmail.com>
2019-09-10 22:26:42 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+//Constant (SABİTLER)
+
+const HEIGHT = 500;
+const WIDTH = 800;
+const PIPE_WIDTH = 50;
+const MIN_PIPE_HEIGHT = 40;
+
+//Pipe Sınıfı
+class Pipe {
+  constructor(ctx, height, space) {
+    //Canvasın alınması
+    this.ctx = ctx;
+    //X koordinatı
+    this.x = 50;
+    //Y Koordinatı
+    this.y = height ? HEIGHT - height : 0;
+    //Genişlik
+    this.width = PIPE_WIDTH;
+    //Math random (0,1)
+    //Uzunluk hesaplanması
+    this.height = height ||  MIN_PIPE_HEIGHT +  Math.random() * (HEIGHT - space - MIN_PIPE_HEIGHT * 2);
+  }
+
+  //Yeni bir pipe çizmek için gerekli fonksiyon
+  draw() {  
+ 
+    //Rect rengi
+    this.ctx.fillStyle = "#000";
+
+    //Üst Pipe
+    this.ctx.fillRect(this.x, this.y, this.width, this.height);
+    //Alt Pipe
+    //this.ctx.fillRect(50,firstPipeHeight + space,PIPE_WIDTH,secondPipeHeight);
+  }
+
+  update() {
+
+  }
+}
+
+
+class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    //Canvas referansının oluşturulması
+    this.canvasRef = React.createRef();
+    //Frame sayacı değişkeni
+    this.frameCount = 0;
+  }
+
+  componentDidMount() {
+    //Canvasın seçilmesi
+    const ctx = this.canvasRef.current.getContext("2d");
+    //Kuşun geçiş aralığı
+    const space = 80;
+    //İlk pipe 'ın oluşturulması
+    const firstPipe = new Pipe(ctx, null, space);
+    //İkinci Pipe'ın uzunluğunun hesaplanması
+    const secondPipeHeight = HEIGHT - firstPipe.height - space;
+    //İkinci Pipe'ın oluşturulması
+    const secondPipe = new Pipe(ctx, secondPipeHeight, space)
+    //Pipes dizisinin oluşturulması
+    this.pipes = [
+      firstPipe,
+      secondPipe
+    ];
+    //Saniyede 60 FPS ile oyun döngüsünün yenilenmesi
+    setInterval(this.gameLopp, 1000 / 60)
+  }
+  
+  //Oyun döngüsünün kurulması
+  gameLopp = () => {
+    this.update()
+    this.draw()
+  }
+
+  //Pipeların oyun alanına çizilmesi
+  draw = () => {
+    this.pipes.forEach(pipe => pipe.draw())
+  }
+
+  update = () => {
+    this.frameCount = this.frameCount + 1;
+    if(this.frameCount % 30 == 0) {
+
+    }
+  }
+
+  
+  render() {
+    return (
+      <div className="App">
+        <canvas
+          ref={this.canvasRef}
+          id="canvas" 
+          width={WIDTH} 
+          height={HEIGHT} 
+          style={{marginTop:"24px",border:"1px solid #000000"}}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Your browser does not support the HTML5 canvas tag.
+        </canvas>
+      </div>
+    )
+  }
 }
 
 export default App;
